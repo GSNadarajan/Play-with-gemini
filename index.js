634213f3b51ec3bada26fc6b93037c6a3b37daf4
GSNadarajan <nadarajangs31@gmail.com>
2024-03-01 10:11:36 +0530

    prompt using image to text converstion using gemini-vision-pro

--- index.js
+++ index.js
@@ -1,20 +1,36 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
+const fs = require("fs");
 require('dotenv').config()
+
 
 // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
+// Converts local file information to a GoogleGenerativeAI.Part object.
+function fileToGenerativePart(path, mimeType) {
+  return {
+    inlineData: {
+      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
+      mimeType
+    },
+  };
+}
+
 async function run() {
-  // For text-only input, use the gemini-pro model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
+  // For text-and-image input (multimodal), use the gemini-pro-vision model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
 
-  const prompt = "Write a story about a magic backpack."
+  const prompt = "What's different between these pictures?";
 
-  const result = await model.generateContent(prompt);
+  const imageParts = [
+    fileToGenerativePart("image1.png", "image/png"),
+    fileToGenerativePart("image2.jpeg", "image/jpeg"),
+  ];
+
+  const result = await model.generateContent([prompt, ...imageParts]);
   const response = await result.response;
   const text = response.text();
   console.log(text);
 }
 
 run();
-
