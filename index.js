7265ae06ebe2c602660a1afea827663cd5589dbc
GSNadarajan <nadarajangs31@gmail.com>
2024-03-01 10:42:23 +0530

    mulit-converstional with gemini-pro model like a chat message

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-const fs = require("fs");
 require('dotenv').config()
 
-
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-// Converts local file information to a GoogleGenerativeAI.Part object.
-function fileToGenerativePart(path, mimeType) {
-  return {
-    inlineData: {
-      data: Buffer.from(fs.readFileSync(path)).toString("base64"),
-      mimeType
-    },
-  };
-}
-
 async function run() {
-  // For text-and-image input (multimodal), use the gemini-pro-vision model
-  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+  // For text-only input, use the gemini-pro model
+  const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const prompt = "What's different between these pictures?";
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: "Hello, I have 2 dogs in my house.",
+      },
+      {
+        role: "model",
+        parts: "Great to meet you. What would you like to know?",
+      },
+    ],
+    generationConfig: {
+      maxOutputTokens: 100,
+    },
+  });
 
-  const imageParts = [
-    fileToGenerativePart("image1.png", "image/png"),
-    fileToGenerativePart("image2.jpeg", "image/jpeg"),
-  ];
+  const msg = "How many legs does the dog has?";
 
-  const result = await model.generateContent([prompt, ...imageParts]);
+  const result = await chat.sendMessage(msg);
   const response = await result.response;
   const text = response.text();
   console.log(text);
--- models/gemini.js
+++ models/gemini.js
@@ -1,7 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 require('dotenv').config()
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
 async function run() {
